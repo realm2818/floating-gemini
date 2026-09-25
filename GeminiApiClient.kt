150a55aa3858f5bea7d0e5e85f58039005b5af8a
realm2818 <tmcrazy01@gmail.com>
2026-09-26 02:30:25 +0700

    Update GeminiApiClient.kt

--- GeminiApiClient.kt
+++ GeminiApiClient.kt
@@ -12,7 +12,7 @@ import java.io.IOException
 
 object GeminiApiClient {
 
-    private const val API_KEY = "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"
+    private const val API_KEY = "OPENROUTER_API_KEY"
 
     private const val URL = "https://openrouter.ai/api/v1/chat/completions"
 
